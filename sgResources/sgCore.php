83e1d3e15e4125972d11e9fce3c72b947c8a73b7
Andrew Pan <andrew.tianyi.pan@gmail.com>
2013-09-03 11:41:32 +0000

    sgCore framework

---
+++ sgResources/sgCore.php
@@ -0,0 +1,19 @@
+<?php
+
+//sgCore version 2.1
+
+class sgCore{
+
+ private $server
+ 
+	function __construct(){
+	  $this->server = ServerAPI::request();
+	}
+	
+	public function init(){
+	  
+	}
+	
+	function __destruct(){
+	
+	} 
