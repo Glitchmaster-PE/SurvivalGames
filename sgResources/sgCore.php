d4be640ca365bf9969c881c1ff8f2ef5184ec27d
Andrew Pan <andrew.tianyi.pan@gmail.com>
2013-09-04 00:39:10 +0000

    Random commit

--- sgResources/sgCore.php
+++ sgResources/sgCore.php
@@ -5,6 +5,17 @@
 class sgCore{
 
  private $server;
+ private $spawnCnt;
+ private $spawnLoc = array(
+   array();
+   array();
+   array();
+   array();
+   array();
+   array();
+   array();
+   array();
+ );
  
 	function __construct(){
 	  $this->server = ServerAPI::request();
